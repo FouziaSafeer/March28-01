e70e52a5bc3dbce2d3fcda7ea4bb14d496c04e31
FouziaSafeer <fouziasafeer8@gmail.com>
2023-03-28 21:42:30 +0100

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
         System.out.println("Hello world01!");
         for (int i = 0; i < 10; i++) {
-            System.out.println(i);
+            System.out.print(i+" ");
 
         }
     }
